0d01de45cd2e71e2c0449bcfc01f5ab1a511c3b7
Vuong <vuong@11.com>
2022-03-05 17:13:24 +0700

    test

---
+++ src/main/java/com/example/tesst/TesstApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tesst/TesstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
